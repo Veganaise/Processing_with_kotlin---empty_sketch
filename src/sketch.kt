1449bd4aad5fd510722d01cb600b385ba5c1f755
antoineG <antoine.ganne@hotmail.fr>
2021-05-01 10:49:18 +0200

    some tweaks before switching to new prototype

--- src/sketch.kt
+++ src/sketch.kt
@@ -2,8 +2,19 @@ import processing.core.PApplet
 import processing.core.PConstants
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import kotlin.math.cos
+import kotlin.math.sin
 
 class MySketch: PApplet(){
+
+    companion object Factory {
+        fun run() {
+            val mySketch = MySketch()
+            mySketch.setSize(1000, 1000)
+
+            mySketch.runSketch()
+        }
+    }
 
     var offset = 0f
 
@@ -27,27 +38,31 @@ class MySketch: PApplet(){
 
     override fun draw(){
         blendMode(PConstants.BLEND)
+        background(color(250, 100, 20))
+        blendMode(PConstants.ADD)
 
 //        fill(color(150,100,100))
 
-        for (x in 100 until 501 step 1) {
-            for (id_line in 0 until 1) {
+        for (x in 300 until 701 step 1) {
+            for (id_line in 0 until 3) {
                 noFill()
-                stroke(color(map(id_line.toFloat(), -2f, 2f, 0f, 360f), 50f, 90f), 100f)
+                stroke(color(map(id_line.toFloat(), 0f, 3f, 0f, 360f), 50f, 90f), 33f)
                 strokeWeight(1.3f)
                 beginShape()
-                for (y in 100 until 500 step 5) {
-                    val easeVal = ease(y.toFloat(), 100f, 500f, 30f)
-                    val n = easeVal * (0.5f - noise((y / 10).toFloat(), offset)) * sin(y / 10f)
-                    val s = easeVal * (sin(y / 100f) * 2 + cos(y / 50f) * 2 + sin(x / 10f) * 0.5f)
-                    val easeX = x + n + s
+                for (y in 200 until 801 step 3) {
+                    val easeVal = ease(y.toFloat(), 200f, 800f, 40f)
+                    val n = easeVal * (0.5f - noise((y / 10).toFloat(), offset, id_line.toFloat()))
+                    val mod2 = if (id_line % 2 < 0.1) 1 else -1
+                    val s =
+                        easeVal * (sin((y * 0.01f * PConstants.TWO_PI)) * mouseX.toFloat() * 0.001f + cos(y * 0.0001f * PConstants.TWO_PI) * mouseY.toFloat() * 0.01f)
+                    val easeX = (x + n + s) * mod2
                     curveVertex(easeX, y.toFloat())
                 }
                 endShape()
                 offset += 0.03f
             }
         }
-        println("here")
+//        println("here")
         noLoop()
     }
 
@@ -58,19 +73,13 @@ class MySketch: PApplet(){
 
     override fun keyPressed() {
         if (key == 's') {
+            kotlin.io.println("saving screenshot")
             save("screenshots/" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd - hhmmss")) + ".png")
         }
     }
 
 
-    companion object Factory{
-        fun run() {
-            val mySketch = MySketch()
-            mySketch.setSize(600,600)
 
-            mySketch.runSketch()
-        }
-    }
 
     fun easeInOutCubic(x: Float): Float {
         return if (x < 0.5f) {
